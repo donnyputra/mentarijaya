d0cc933dbb6ec5bee58dc90c6f874b1307be6534
Donny Putra Perdana <donny.p.perdana@gmail.com>
2020-03-20 00:39:17 +0700

    Change title in book & preferences page

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -5,7 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
 
-            <h1>{{ __('BOOK') }}</h1>
+            <h3>
+                {{ __("Book") }}
+                <small class="text-muted">{{ __("- Manage items detail, price, sales, etc.") }}</small>
+            </h3>
 
             @if (session('status'))
                 <div class="alert alert-success" role="alert">
--- resources/views/preferences.blade.php
+++ resources/views/preferences.blade.php
@@ -4,8 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            
-            <h1>{{ __('SYSTEM PREFERENCES') }}</h1>
+
+            <h3>
+                {{ __("System Preferences") }}
+                <small class="text-muted">{{ __("- Manage list of status, type, category, etc.") }}</small>
+            </h3>
 
             @if (session('status'))
                 <div class="alert alert-success" role="alert">
