a1c4cc38d43048d3a4d4b185cc4f59ca540dbb70
Donny Putra Perdana <donny.p.perdana@gmail.com>
2020-03-20 00:42:02 +0700

    Add condition in displaying header menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,12 +33,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        @auth
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('book') }}">{{ __('Book') }}</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('preferences') }}">{{ __('System Preferences') }}</a>
                         </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
