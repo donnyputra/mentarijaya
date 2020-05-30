92fb2353147b7fc57033e6b0bdbaa2c3c13bc52a
Donny Putra Perdana <donny.p.perdana@gmail.com>
2020-05-30 21:31:18 +0700

    Fix user seeder script

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,7 +12,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // DB::table('users')->truncate();
+        DB::table('users')->truncate();
         $currentTimestamp = now();
         
         DB::table('users')
