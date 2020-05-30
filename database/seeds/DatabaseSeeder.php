a4e491a983fc5f56148b98e81b2aab6a9bbedcdd
Donny Putra Perdana <donny.p.perdana@gmail.com>
2020-05-30 21:28:55 +0700

    Create user seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(ItemStatusSeeder::class);
         $this->call(SalesStatusSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table('users')->truncate();
+        $currentTimestamp = now();
+        
+        DB::table('users')
+            ->insert(
+                [
+                    'name' => 'System Administrator',
+                    'username' => 'admin',
+                    'email' => 'admin@admin.com',
+                    'password' => bcrypt('admin'),
+                    'created_at' => $currentTimestamp,
+                    'updated_at' => $currentTimestamp,
+                ]
+            );
+    }
+}
