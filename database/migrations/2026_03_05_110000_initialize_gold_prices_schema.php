c28487577721b29cfc5a9ad5f13edf11a1b9be5e
Christian Dwi Ananta <christiandwi13@gmail.com>
2026-06-17 23:36:51 +0800

    fix gold_prices migration collision

--- database/migrations/2026_03_05_110000_initialize_gold_prices_schema.php
+++ database/migrations/2026_03_05_110000_initialize_gold_prices_schema.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGoldPricesTableNormalizedSchema extends Migration
+class InitializeGoldPricesSchema extends Migration
 {
     /**
      * Run the migrations.
